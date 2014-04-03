c45ef057b4988adffec07e8d4589a047ee32310e
crowdcat <catherine@crowdflower.com>
2014-04-02 22:04:15 -0700

    Making proper_case.R more legible

--- proper_case.R
+++ proper_case.R
@@ -1,21 +1,21 @@
+toproper <- function(x) {
+    # Makes Proper Capitalization out of a string or collection of strings.
+    sapply(x, function(strn) {
+        s <- strsplit(strn, "\\s")[[1]]
+        return(paste0(toupper(substring(s, 1,1)),
+        tolower(substring(s, 2)),
+        collapse=" "))
+    }, USE.NAMES=FALSE)
+}
+
+
 proper_case <- function(file, cols){
-
-	toproper <- function(x) {
-     # Makes Proper Capitalization out of a string or collection of strings. 
-     sapply(x, function(strn)
-     { s <- strsplit(strn, "\\s")[[1]]
-       paste0(toupper(substring(s, 1,1)), 
-              tolower(substring(s, 2)),
-              collapse=" ")}, USE.NAMES=FALSE)
-		}
-	
-		for(j in 1:length(cols)){
-			for(i in 1:nrow(file)){
-				row = i
-				column = cols[j]
-				file[row, column] = toproper(file[row, column])
-
-			}
-		}
-		return(file)
+    for(j in 1:length(cols)){
+        for(i in 1:nrow(file)){
+            row = i
+            column = cols[j]
+            file[row, column] = toproper(file[row, column])
+        }
+    }
+    return(file)
 }
